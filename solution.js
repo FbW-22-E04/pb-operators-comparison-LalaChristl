7a5956de700b4379c477bfc09332cd8b20ba6c02
LalaChristl <lesleyannchristl@gmail.com>
2022-06-29 15:35:09 +0200

    added my work to solution.js

---
+++ solution.js
@@ -0,0 +1,27 @@
+let apples = '20';
+let oranges = '30';
+console.log(apples == oranges)
+console.log(apples != oranges)
+console.log(apples > oranges)
+console.log(apples >= oranges)
+console.log(oranges > apples)
+
+let mangoes = '5';
+console.log(mangoes * apples)
+console.log(mangoes + oranges)
+console.log(mangoes + oranges > mangoes * apples)
+console.log(apples - mangoes)
+console.log(apples - mangoes < oranges / mangoes)
+console.log(mangoes == apples == oranges)
+console.log(apples)
+console.log(apples / mangoes % oranges / mangoes)
+console.log(apples / mangoes)
+console.log(oranges / mangoes)
+console.log(apples == oranges)
+console.log(mangoes + apples)
+console.log(oranges - mangoes)
+console.log(apples > oranges)
+console.log(apples != oranges)
+console.log(apples)
+console.log(oranges)
+console.log("Is it true that I have more apples than oranges?", + "The answer is:" + apples == oranges);
